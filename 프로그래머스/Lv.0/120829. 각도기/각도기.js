9cb78caa99199a8c0d7c631bcd0a17bb7a098fb9
MinA <97724189+mineii@users.noreply.github.com>
2024-01-15 23:46:02 +0900

    [level Lv.0] Title: 각도기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120829. 각도기/각도기.js
@@ -0,0 +1,13 @@
+function solution(angle) {
+    var answer = 0;
+    if (angle < 90 && angle > 0) {
+        return answer = 1;
+    } else if (angle == 90 ) {
+        return answer = 2;
+    } else if (angle > 90 && angle < 180 ) {
+        return answer = 3;
+    } else if (angle == 180 ) {
+        return answer = 4;
+    }
+    return answer;
+}
