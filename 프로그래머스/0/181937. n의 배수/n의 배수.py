d463e499961713f74c2969db817a4982557cac31
MinA <97724189+mineii@users.noreply.github.com>
2024-02-07 08:43:03 +0900

    [level 0] Title: n의 배수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.py
@@ -0,0 +1,10 @@
+def solution(num, n):
+    answer = 0
+    
+    if num % n == 0 :
+        answer = 1
+        return answer
+    else :
+        answer = 0
+        return answer
+    return answer
